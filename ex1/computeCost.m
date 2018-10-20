73ba40f01da2d0fa2a9f382280950a542089a138
Eduardo Silva <eduardocs09@gmail.com>
2018-10-20 17:49:08 -0300

    Plotting data and computing cost function

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+predictions = X * theta;
+difference = predictions - y;
+J = sum(difference.^2)/(2*m);
 
 % =========================================================================
 
